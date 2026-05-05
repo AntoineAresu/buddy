ae0b251d83d55c0c49c0d6f5f8de9485a0f7c92f
Antoine-HP <aresu.antoine@gmail.com>
2026-05-05 12:22:39 +0200

    :lock: [CalendarController] Check permission on show

--- src/Controller/CalendarController.php
+++ src/Controller/CalendarController.php
@@ -2,18 +2,19 @@
 
 namespace App\Controller;
 
+use App\Entity\Dog;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 final class CalendarController extends AbstractController
 {
-    #[Route('/calendar', name: 'show_calendar')]
-    #[IsGranted('IS_AUTHENTICATED_FULLY')]
-    public function calendar(): Response
+    #[Route('/dog/{id<\d+>}/calendar', name: 'show_calendar')]
+    #[IsGranted('UPDATE', 'dog')]
+    public function calendar(Dog $dog): Response
     {
         return $this->render('calendar/show.html.twig', [
-            'dog' => $this->getUser()?->getDogs()->first(),
+            'dog' => $dog,
         ]);
     }
 }
