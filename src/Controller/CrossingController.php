342ef76a309843ce2534e5b67bc4975a6472270a
Antoine-HP <aresu.antoine@gmail.com>
2026-05-12 13:10:54 +0200

    :sparkles: [CrossingType] Add form and route

---
+++ src/Controller/CrossingController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Crossing;
+use App\Entity\Dog;
+use App\Form\CrossingType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+final class CrossingController extends AbstractController
+{
+    #[Route('/dog/{id}/day/{date<\d{4}-\d{2}-\d{2}>}/crossing/create', name: 'create_crossing')]
+    #[IsGranted('UPDATE', 'dog')]
+    public function create(Request $request, Dog $dog, ?\DateTime $date, EntityManagerInterface $em): Response
+    {
+        $crossing = new Crossing()->setDog($dog)->setDate($date);
+        $form = $this->createForm(CrossingType::class, $crossing)->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($crossing);
+            $em->flush();
+
+            return $this->redirectToRoute('show_calendar_day', [
+                'id' => $dog->getId(),
+                'date' => $date->format('Y-m-d'),
+            ]);
+        }
+
+        return $this->render('crossing/index.html.twig', [
+            'dog' => $dog,
+            'form' => $form,
+        ]);
+    }
+}
