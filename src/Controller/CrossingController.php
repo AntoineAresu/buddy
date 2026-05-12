8631c6670e983e693ad437ba5a571f965dff24bd
Antoine-HP <aresu.antoine@gmail.com>
2026-05-12 14:03:18 +0200

    :recycle: [RouteFormat] Improve routes format

--- src/Controller/CrossingController.php
+++ src/Controller/CrossingController.php
@@ -8,16 +8,23 @@ use App\Form\CrossingType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 final class CrossingController extends AbstractController
 {
-    #[Route('/dog/{id}/day/{date<\d{4}-\d{2}-\d{2}>}/crossing/create', name: 'create_crossing')]
+    #[Route('/dog/{id}/crossing/create', name: 'create_crossing')]
     #[IsGranted('UPDATE', 'dog')]
-    public function create(Request $request, Dog $dog, ?\DateTime $date, EntityManagerInterface $em): Response
-    {
-        $crossing = new Crossing()->setDog($dog)->setDate($date ?? new \DateTime());
+    public function create(
+        Request $request,
+        Dog $dog,
+        #[MapQueryParameter] string $date,
+        EntityManagerInterface $em,
+    ): Response {
+        $date = $this->getDateFromQuery($date);
+
+        $crossing = new Crossing()->setDog($dog)->setDate($date);
         $form = $this->createForm(CrossingType::class, $crossing)->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
@@ -26,7 +33,7 @@ final class CrossingController extends AbstractController
 
             return $this->redirectToRoute('show_calendar_day', [
                 'id' => $dog->getId(),
-                'date' => $date?->format('Y-m-d'),
+                'date' => $date->format('Y-m-d'),
             ]);
         }
 
