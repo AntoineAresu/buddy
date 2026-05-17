082c16ca65e615b9cda3eef9a144b8d2eb0549f8
Antoine-HP <aresu.antoine@gmail.com>
2026-05-17 22:22:57 +0200

    :fire: [docker] remove

--- deploy.php
+++ deploy.php
@@ -19,29 +19,4 @@ host('buddy_pp')
     ->set('branch', 'feature/docker')
     ->set('deploy_path', '/var/www/buddy_pp');
 
-// Tasks
-
-task('docker:build', function () {
-    run('cd {{release_path}} && make build_pp');
-});
-
-task('docker:up', function () {
-    run('cd {{release_path}} && make up_pp');
-});
-
-task('docker:migrate', function () {
-    run('cd {{release_path}} && make migrate');
-});
-
-task('docker:down', function () {
-    run('cd {{previous_release}} && make down_pp || true');
-});
-
-task('deploy:vendors')->disable();
-// Hooks
-
-after('deploy:symlink', 'docker:build');
-after('docker:build', 'docker:down');
-after('docker:down', 'docker:up');
-after('docker:up', 'docker:migrate');
 after('deploy:failed', 'deploy:unlock');
